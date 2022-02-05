6abc236483f47e37ae4205b20327986797f07d7c
Amit P Naik <amit.naik8103@gmail.com>
2022-02-05 13:49:53 +0530

    Updates

---
+++ precache-manifest.d6645b4100962aa9fa5f48e45bd6d6a9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82d6665573492d90a867d2679cc6206",
+    "url": "/bhagavad-gita/index.html"
+  },
+  {
+    "revision": "86bb00460d0fea220816",
+    "url": "/bhagavad-gita/static/css/2.8280a61e.chunk.css"
+  },
+  {
+    "revision": "764c04269e8f04ec6205",
+    "url": "/bhagavad-gita/static/css/main.762299d0.chunk.css"
+  },
+  {
+    "revision": "86bb00460d0fea220816",
+    "url": "/bhagavad-gita/static/js/2.27fe0bde.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/bhagavad-gita/static/js/2.27fe0bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764c04269e8f04ec6205",
+    "url": "/bhagavad-gita/static/js/main.a0823e4a.chunk.js"
+  },
+  {
+    "revision": "60aa9b835e592e4f964e",
+    "url": "/bhagavad-gita/static/js/runtime-main.5ee345e4.js"
+  },
+  {
+    "revision": "49611f2982d0e8ef61372afe9850535b",
+    "url": "/bhagavad-gita/static/media/CircularStd-Black.49611f29.woff"
+  },
+  {
+    "revision": "1c46c3018afa9b775e395b2076943ede",
+    "url": "/bhagavad-gita/static/media/CircularStd-BlackItalic.1c46c301.woff"
+  },
+  {
+    "revision": "03e7ee16210905484b09316e8298d712",
+    "url": "/bhagavad-gita/static/media/CircularStd-Bold.03e7ee16.woff"
+  },
+  {
+    "revision": "705bbdf2db3c4668c8750804ca12d962",
+    "url": "/bhagavad-gita/static/media/CircularStd-BoldItalic.705bbdf2.woff"
+  },
+  {
+    "revision": "829012af195c90c00bac2953653768a8",
+    "url": "/bhagavad-gita/static/media/CircularStd-Book.829012af.woff"
+  },
+  {
+    "revision": "3c7409b8c5420ebd6022acae44779108",
+    "url": "/bhagavad-gita/static/media/CircularStd-BookItalic.3c7409b8.woff"
+  },
+  {
+    "revision": "381f87a977e09572d69be5e7f6087fe4",
+    "url": "/bhagavad-gita/static/media/CircularStd-Medium.381f87a9.woff"
+  },
+  {
+    "revision": "451b6f03fc497603eebc23d24a97bbb0",
+    "url": "/bhagavad-gita/static/media/CircularStd-MediumItalic.451b6f03.woff"
+  },
+  {
+    "revision": "8d4ab8eaf89e12a2cf91e01b25e0a6c1",
+    "url": "/bhagavad-gita/static/media/card-img.8d4ab8ea.jpg"
+  },
+  {
+    "revision": "42e252b2b0c71a9613e1cb7b39108c58",
+    "url": "/bhagavad-gita/static/media/logo3.42e252b2.png"
+  }
+]);
